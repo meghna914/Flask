a6bff73c9b82580050fddb92951c1f87f6fc3ec3
meghna914 <mmeghna958@gmail.com>
2025-01-26 17:39:03 +0530

    Update app.py

--- FirstApp/app.py
+++ FirstApp/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!!</h1>"
 
+@app.route('/hello')
+def hello():
+    return "hey there! welcome to the world of flask!!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug = True)
