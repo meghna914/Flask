b432aa74c7fe7ba1f7d9c971763e429bc9aa774a
meghna914 <mmeghna958@gmail.com>
2025-01-27 22:03:22 +0530

    Update app.py

--- FirstApp/app.py
+++ FirstApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,23 @@ def index():
 @app.route('/hello')
 def hello():
     return "hey there! welcome to the world of flask!!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}! Hope you are doing well!!"
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return f"<h2>The sum of the two numbers are as follows:</h2>\n{num1} + {num2} = {num1+num2}"
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args.get('greeting')
+        name = request.args.get('name')
+        return f"{greeting}, {name}! :)"
+    else:
+        return "Some parameters are missing :("
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug = True)
